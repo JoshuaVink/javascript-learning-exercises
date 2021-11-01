10e9f908cbe3e90bab1e391c6bdeed9d584db43d
Joshua Vink <joshuavink34@gmail.com>
2021-11-01 19:02:26 -0400

    starting work on FizzBuzz

---
+++ FizzBuzz.js
@@ -0,0 +1,16 @@
+let number = 1;
+while (number < 100) {
+    if (number % 5 == 0) {
+    console.log("Buzz");
+    //let number =number + 1;
+    }
+    else if (number % 3 == 0) {
+    console.log("Fizz"); 
+    //let number = number + 1;
+    break;
+    }
+    else {
+    console.log(number) 
+   // let number = number + 1;
+    }
+}
