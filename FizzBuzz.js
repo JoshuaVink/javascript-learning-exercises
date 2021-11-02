f07873f4f57024c15a1afbc0bafee63ac3e7d5a7
Joshua Vink <joshuavink34@gmail.com>
2021-11-01 22:01:16 -0400

    Tried to finish, got uncaught refernce error

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,15 +2,15 @@ let number = 1;
 while (number < 100) {
     if (number % 5 == 0) {
     console.log("Buzz");
-    //let number =number + 1;
+    let number =number + 1;
     }
     else if (number % 3 == 0) {
     console.log("Fizz"); 
-    //let number = number + 1;
+    let number = number + 1;
     break;
     }
     else {
     console.log(number) 
-   // let number = number + 1;
+    let number = number + 1;
     }
 }
