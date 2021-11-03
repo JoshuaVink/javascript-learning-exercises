d7425538dba82a235e10a16f5ba00a7abda2fac9
Joshua Vink <joshuavink34@gmail.com>
2021-11-03 17:09:24 -0400

    fixed/finished FizzBuzz.
    removed let from subsequent use of 'number', and added FizzBuzz for multiples of both 3 and 5

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,16 +1,19 @@
 let number = 1;
-while (number < 100) {
-    if (number % 5 == 0) {
+while (number < 101) {
+  if (number % 5 == 0 && number % 3 == 0) {
+    console.log("FizzBuzz");
+    number = number + 1;
+}
+    else if (number % 5 == 0) {
     console.log("Buzz");
-    let number =number + 1;
-    }
-    else if (number % 3 == 0) {
-    console.log("Fizz"); 
-    let number = number + 1;
-    break;
-    }
-    else {
-    console.log(number) 
-    let number = number + 1;
-    }
+    number = number + 1;
+  } 
+  else if (number % 3 == 0) {
+    console.log("Fizz");
+    number = number + 1;
+  } 
+  else {
+    console.log(number);
+    number = number + 1;
+  }
 }
